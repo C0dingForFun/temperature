f29bef004652b15c12cb1472d11de535ae41740a
C0dingForFun <gwibantulini8@gmail.com>
2024-05-17 20:46:26 +0200

    temperature

--- Script/temperature.js
+++ Script/temperature.js
@@ -1,7 +1,8 @@
 let celcius = document.querySelector('#celcius');
 let answer = document.querySelector('#answer');
-let calc = document.querySelector('#calc');
+let calc = document.querySelector('.calc');
 
 calc.addEventListener('click', ()=>{
     answer.innerText = parseFloat(eval(`(${celcius.value} * 9/5) + 32`)).toFixed(2);
 })
+
