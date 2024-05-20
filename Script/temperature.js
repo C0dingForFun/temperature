0e006ba35e61b62fd17770ec0a1b687bb5b45591
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-05-20 11:54:12 +0200

    A okay

--- Script/temperature.js
+++ Script/temperature.js
@@ -1,8 +1,8 @@
-let celcius = document.querySelector('#celcius');
+let celsius = document.querySelector('#celsius');
 let answer = document.querySelector('#answer');
 let calc = document.querySelector('.calc');
 
 calc.addEventListener('click', ()=>{
-    answer.innerText = parseFloat(eval(`(${celcius.value} * 9/5) + 32`)).toFixed(2);
+    answer.innerText = parseFloat(eval(`(${celsius.value} * 9/5) + 32`)).toFixed(2) + "°F";
 })
 
